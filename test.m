a4ceeaa98720ed8998031234628abd9b028f5b63
neusxw <36007456+neusxw@users.noreply.github.com>
2018-07-27 02:40:27 +0800

    在避障条件下画线--finish

--- test.m
+++ test.m
@@ -2,5 +2,7 @@ clc;
 clear all;
 xv=[0.2 0.5 0.6 0.1];
 yv=[0.7 0.8 0.3 0.4];
+boundary=[xv;yv];
+barrier=[0.4 0.5 0.5 0.4;0.5 0.5 0.4 0.4];
 Lines=[];
-[Lines] = addPatch2Line(xv,yv,Lines)
+Lines = addPatch2Line(boundary,barrier,Lines)
