cb413eae3f434d5c457c9fbaa065e38e3a89a634
neusxw <36007456+neusxw@users.noreply.github.com>
2018-12-30 00:38:08 +0800

    Create JavaTest.m

---
+++ JavaTest.m
@@ -0,0 +1,34 @@
+clc;
+clear all;
+close all;
+figure;
+hold on;
+axis equal
+axis([-1 1 0 1]);
+xv1=[0.2 0.6 0.7 0.1 0.2];
+yv1=[0.7 0.75 0.3 0.4 0.7];
+plot(xv1,yv1);
+xv2=[-0.9 -0.7 -0.3 -0.4 -0.8 -0.9];
+yv2=[0.9 0.9 0.7 0.5 0.6 0.9];
+plot(xv2,yv2);
+aBarrier=[0.4 0.5 0.5 0.4;0.5 0.5 0.4 0.4];
+fill(aBarrier(1,:),aBarrier(2,:),'black');
+aBarrier=[0.2 0.3 0.35 0.2; 0.6 0.6 0.5 0.5];
+fill(aBarrier(1,:),aBarrier(2,:),'black');
+aBarrier=[-0.6 -0.4 -0.4 -0.6; 0.3 0.3 0.1 0.1];
+fill(aBarrier(1,:),aBarrier(2,:),'black');
+aBarrier=[-0.7 -0.6 -0.6 -0.7;0.8 0.8 0.6 0.6];
+fill(aBarrier(1,:),aBarrier(2,:),'black');
+a=load('D:\github\Java\JavaUAV\output\linesOut.txt')
+for i=1:size(a,1)
+    plot([a(i,1) a(i,3)],[a(i,2) a(i,4)])
+end
+b=load('D:\github\Java\JavaUAV\output\pointsOut.txt');
+for i=1:size(b,1)
+    plot(b(i,1),b(i,2),'r*');
+end
+for i=1:size(b,1)-1
+    plot([b(i,1),b(i+1,1)],[b(i,2),b(i+1,2)],'r','LineWidth',4);
+    getframe
+    pause(0.5)
+end
